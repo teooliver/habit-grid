961ec9bc51605b5748943d78fa17861ed1804e33
Teo Oliver <teo.liz.oli@gmail.com>
2020-06-30 17:55:35 +0100

    inputRef for focus

--- src/components/tracker/HabitForm.tsx
+++ src/components/tracker/HabitForm.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useEffect, useRef } from "react";
+import React, { FC, useState, useEffect, useRef, useLayoutEffect } from "react";
 import { connect } from "react-redux";
 import { createHabit } from "../../redux/actions/habits";
 import PlusCircleIcon from "../layout/PlusCircleIcon";
@@ -10,7 +10,8 @@ const HabitForm: FC<Props> = ({ createHabit }) => {
   const [open, setOpen] = useState(false);
   const [habitName, setHabitName] = useState("");
   const [isValid, setIsValid] = useState(true);
-  const formRef = useRef<HTMLDivElement>(null);
+  const habitFormRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const inputValidation = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value === "") {
@@ -19,6 +20,7 @@ const HabitForm: FC<Props> = ({ createHabit }) => {
     return setIsValid(true);
   };
 
+  // TODO: Implement working validation for empty strings
   const handleSubmit = () => {
     if (isValid) {
       createHabit(habitName);
@@ -34,10 +36,17 @@ const HabitForm: FC<Props> = ({ createHabit }) => {
     };
   }, []);
 
+  useLayoutEffect(() => {
+    if (open) inputRef.current?.focus();
+  }, [open]);
+
   const handleClickOutside: Function = (
     e: React.ChangeEvent<HTMLInputElement>
   ) => {
-    if (formRef.current != null && !formRef.current.contains(e.target)) {
+    if (
+      habitFormRef.current != null &&
+      !habitFormRef.current.contains(e.target)
+    ) {
       setOpen(false);
     } else {
       return;
@@ -45,13 +54,19 @@ const HabitForm: FC<Props> = ({ createHabit }) => {
   };
 
   return (
-    <div ref={formRef} className="HabitForm">
-      <div onClick={() => setOpen(!open)}>
+    <div ref={habitFormRef} className="HabitForm">
+      <div
+        onClick={() => {
+          setOpen(!open);
+          setHabitName("");
+        }}
+      >
         <PlusCircleIcon className="plus-circle" />
       </div>
       {open && (
         <form>
           <input
+            ref={inputRef}
             type="text"
             id="name"
             required
