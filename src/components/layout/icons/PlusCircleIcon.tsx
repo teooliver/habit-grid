0bd808c8be55a9e88427638bee785b75bb2c1347
Teo Oliver <teo.liz.oli@gmail.com>
2020-07-04 16:48:31 +0100

    fixed svg tag names

--- src/components/layout/icons/PlusCircleIcon.tsx
+++ src/components/layout/icons/PlusCircleIcon.tsx
@@ -15,7 +15,7 @@ const PlusCircleIcon: FC<Props> = ({ className }) => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4a.5.5 0 0 0-1 0v3.5H4a.5.5 0 0 0 0 1h3.5V12a.5.5 0 0 0 1 0V8.5H12a.5.5 0 0 0 0-1H8.5V4z"
       />
     </svg>
