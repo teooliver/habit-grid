a3b1c27c0d894e99f4bece746526ddee848df01f
Teo Oliver <teo.liz.oli@gmail.com>
2020-07-08 17:30:56 +0100

    back to default CRA service worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,23 +1,7 @@
 const staticCacheName = "version-v2";
-// const assets = [
-//   "/",
-//   "./index.html",
-//   "./static",
-//   "./static/css/main.3e6cf5ff.chunk.css",
-//   "./static/css/main.3e6cf5ff.chunk.css.map",
-//   "./static/js/2.cb426b82.chunk.js",
-//   "./static/js/2.cb426b82.chunk.js.LICENSE.txt",
-//   "./static/js/2.cb426b82.chunk.js.map",
-//   "./static/js/main.5ede2531.chunk.js",
-//   "./static/js/main.5ede2531.chunk.js.map",
-//   "./static/js/runtime-main.bccd1c6d.js",
-//   "./static/js/runtime-main.bccd1c6d.js.map",
-//   "./static/media/undraw_Stability_ball_b4ia.4cb18637.svg",
-// ];
-
 const assets = ["/", "./index.html", "./static"];
 
-self = this;
+// self = this;
 
 // Install SW
 self.addEventListener("install", (event) => {
@@ -50,9 +34,9 @@ self.addEventListener("activate", (event) => {
 // Listen for requests
 self.addEventListener("fetch", (event) => {
   //   console.log("Fetch event", event);
-  event.respondWith(
-    caches.match(event.request).then((cacheRes) => {
-      return cacheRes || fetch(event.request);
-    })
-  );
+  //   event.respondWith(
+  //     caches.match(event.request).then((cacheRes) => {
+  //       return cacheRes || fetch(event.request);
+  //     })
+  //   );
 });
--- src/index.tsx
+++ src/index.tsx
@@ -31,5 +31,5 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
-serviceWorker.unregister();
-// serviceWorker.register();
+// serviceWorker.unregister();
+serviceWorker.register();
