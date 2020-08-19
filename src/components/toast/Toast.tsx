aee149a4d562d8caabba5933a02ebf1407a30114
Teo Oliver <teo.liz.oli@gmail.com>
2020-08-19 20:13:31 +0100

    initial Toast setup

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Toast = () => {
-    return (
-        <div className="Toast">
-            <p>Oops!</p>
-            <p>Error, this is a error, be careful!</p>
-            <button>X</button>
-        </div>
-    )
-}
+  return (
+    <div className='Toast'>
+      <p className='toast_title'>Oops!</p>
+      <p className='toast_body'>Error, this is a error, be careful!</p>
+      <button>X</button>
+    </div>
+  );
+};
 
-export default Toast
+export default Toast;
