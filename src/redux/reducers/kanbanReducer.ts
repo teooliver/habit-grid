1ea315dc94af7eb0935446e9586d9736fe4a0098
Teo Oliver <teo.liz.oli@gmail.com>
2020-08-23 20:12:02 +0100

    inital Kanban Reducer implementation

--- src/redux/reducers/kanbanReducer.ts
+++ src/redux/reducers/kanbanReducer.ts
@@ -1,7 +1,41 @@
 import { ActionTypes, Board, KanbanActions } from "../actions/index";
 
+const initialState: Board[] = [
+  {
+    id: 329840982,
+    name: "TestBoard",
+    columnns: ["todo", "in progress", "done"],
+    issues: [
+      {
+        id: "1",
+        title: "Read Something",
+        description: "Read Something for school project",
+        boardId: 329840982,
+        column: "todo",
+      },
+      {
+        id: "2",
+        title: "Read Something",
+        description: "Read Something for school project",
+        boardId: 329840982,
+        column: "todo",
+      },
+      {
+        id: "3",
+        title: "Read Something",
+        description: "Read Something for school project",
+        boardId: 329840982,
+        column: "todo",
+      },
+    ],
+  },
+];
+
 // Todo Create KanbanActions
-export const kanbanReducer = (state: Board[] = [], action: KanbanActions) => {
+export const kanbanReducer = (
+  state: Board[] = initialState,
+  action: KanbanActions
+) => {
   switch (action.type) {
     case ActionTypes.getKanbanBoards:
       return state;
