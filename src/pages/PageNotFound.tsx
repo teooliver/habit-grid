ca48924624fb23421ebd25a45e480ea430f7f272
Teo Oliver <teo.liz.oli@gmail.com>
2020-08-24 19:52:58 +0100

    added the new components and initial scaffolding

---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface PageNotFoundProps {
+
+}
+
+export const PageNotFound: React.FC<PageNotFoundProps> = ({}) => {
+    return (<div className='PageNotFound'></div>);
+}
