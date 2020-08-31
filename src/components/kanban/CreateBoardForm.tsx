1ff5bdec6ef7a02c6e46285a1e4c02e113929e62
Teo Oliver <teo.liz.oli@gmail.com>
2020-08-31 23:32:35 +0100

    eod

--- src/components/kanban/CreateBoardForm.tsx
+++ src/components/kanban/CreateBoardForm.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { createBoard } from '../../redux/actions';
 
@@ -6,32 +6,41 @@ interface CreateBoardFormProps {
   createBoard: Function;
 }
 
-const initialState = {
-  boardName: '',
-  columnsNames: [],
-};
-
-const formReducer = (formState, action) => {
-  switch (action.type) {
-    case 'changeBoardName':
-      return action.payload;
-  }
-};
-
 const CreateBoardForm: React.FC<CreateBoardFormProps> = ({ createBoard }) => {
-  const [formState, dispatch] = useReducer(formReducer, initialState);
+  const [boardName, setBoardName] = useState<string>();
+  const [columnNames, setColumnNames] = useState<string[]>([]);
 
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    createBoard(formState);
+    createBoard({ boardName, columnNames });
+  };
+
+  const onChangeBoardName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBoardName(e.target.value);
+  };
+
+  const onChangeColumnName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    const columnsArray = e.target.value.split(',');
+    setColumnNames(columnsArray);
   };
 
   return (
-    <form className="CreateBoardForm">
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
+    <form className="CreateBoardForm" onSubmit={onSubmit}>
+      <input
+        type="text"
+        name="boardName"
+        value={boardName}
+        onChange={(e) => onChangeBoardName(e)}
+      />
+      <input
+        type="text"
+        name="columnsName"
+        value={columnNames}
+        onChange={(e) => onChangeColumnName(e)}
+      />
+      <button>Submit</button>
     </form>
   );
 };
