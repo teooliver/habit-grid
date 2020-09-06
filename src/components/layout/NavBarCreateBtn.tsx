c589bdef2916c054c75b818e54da14005c3061e6
Teo Oliver <teo.liz.oli@gmail.com>
2020-09-06 21:33:04 +0100

    refactor: NavBarCreateBtn

--- src/components/layout/NavBarCreateBtn.tsx
+++ src/components/layout/NavBarCreateBtn.tsx
@@ -7,10 +7,14 @@ interface NavBarCreateBtnProps {}
 export const NavBarCreateBtn: React.FC<NavBarCreateBtnProps> = ({}) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const handleOnClick = () => {
+    setIsModalOpen(true);
+  };
+
   return (
     <>
       <div className="NavBarCreateBtn">
-        <button onClick={() => setIsModalOpen(true)}>Create Board</button>
+        <button onClick={() => handleOnClick()}>Create Board</button>
       </div>
       <Portal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
         <CreateBoardForm
